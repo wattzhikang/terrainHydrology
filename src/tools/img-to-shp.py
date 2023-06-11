94ca30af9b8b4e80a3baf9a85d2dc514f6607b6a
wattzhikang <13468523.wattzhikang@gmail.com>
2023-06-11 14:17:49 -0500

    some cleanup issue-61

--- src/tools/img-to-shp.py
+++ src/tools/img-to-shp.py
@@ -52,6 +52,9 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+# Transforming from image coordinates to real-world coordinates can be a simple function,
+# but it requires a few parameters that are specific to the image. This decorator returns
+# a function that does that, but with the paramters already set.
 def getCoordinateTransformFunction(imgSize: Tuple[float,float], resolution: float):
     def fromImageCoordinates(loc: Tuple[float,float]):
         x = loc[0]
@@ -72,19 +75,22 @@ longitude = args.longitude
 resolution = args.resolution
 outputFile = args.outputFile
 
+# Read the image and convert it to grayscale
 img = cv.imread(gammaFileName)
+imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # a black-and-white version of the input image
 rasterShape = imgray.shape
-realShape = (imgray.shape[0] * resolution, imgray.shape[1] * resolution)
+
+# Find the contours in the image
 ret, thresh = cv.threshold(imgray, 127, 255, 0)
 contours, hierarchy = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+
+# We assume that the first contour is the shoreline. If there are multiple contours, there's no way to know which is the shoreline, anyway.
 if len(contours) > 1:
     print('WARNING: Multiple contours identified. The program may not have correctly')
     print('identified the shoreline.')
-
-# We assume that the first contour is the shoreline. If there are multiple contours, there's no way to know which is the shoreline, anyway.
 contour = contours[0]
+
 # The shape of the contour array is (N, 1, 2), where N is the number of points in the contour. That middle dimension is unnecessary, so we remove it.
 contour=contour.reshape(-1,2)
 # Now the array is (N, 2).
@@ -92,27 +98,21 @@ contour=contour.reshape(-1,2)
 # But the points are in counterclockwise order. Pyshp expects clockwise order, so we flip the array on that axis.
 contour=np.flip(contour,0)
 
-# fromImageCoordinates(
-#     (self.contour[index][1],self.contour[index][0]),
-#     self.imgray.shape,
-#     self.resolution)
-
-print(f'Before transform: {contour}')
 # transform the contour to real-world coordinates
 transform = getCoordinateTransformFunction(rasterShape, resolution)
 contour = np.array([transform(loc) for loc in contour])
-print(f'After transform: {contour}')
 
 # TODO raise exception if dimensions not square
 # TODO raise exception if multiple contours
 
+# Write the shapefile
 with shapefile.Writer(outputFile, shapeType=5) as shp:
     shp.field('name', 'C')
 
     shp.poly([ contour.tolist() ])
     shp.record('polygon0')
 
-## Create the .prj file to be read by GIS software
+# Write the .prj file to be read by GIS software
 with open(f'{outputFile}.prj', 'w') as prj:
     prjstr = f'PROJCS["unknown",GEOGCS["GCS_unknown",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Orthographic"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Longitude_Of_Center",{args.longitude}],PARAMETER["Latitude_Of_Center",{args.latitude}],UNIT["Meter",1.0]]'
     prj.write(prjstr)
